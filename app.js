28988ad7017b527b8994b4a5f2daf2500a0baa2c
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-03-05 15:22:43 +0200

    added a check for valid url and hostname

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const fs = require("fs");
+const isValidHostname = require("is-valid-hostname");
 
 const ID = 0;
 app.use(cors());
@@ -10,6 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use("/public", express.static(`./public`));
+
+function isUrlValid(url) {
+  var res = url.match(
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
+  );
+  if (res == null) return false;
+  else return true;
+}
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
@@ -27,7 +36,7 @@ app.get("/api/shorturl/:shortUrl", (req, res) => {
       res.redirect(url);
     }
   }
-  res.send("No shortened url found in the database");
+  res.send("<h1>No shortened url found in the database</h1>");
 });
 
 app.post("/DataBase/database.json", (req, res) => {
@@ -45,17 +54,23 @@ app.post("/DataBase/database.json", (req, res) => {
   if (isExist) {
     res.send("url already exist!");
   } else {
-    parseData.push(body);
-    fs.writeFile(
-      "./DataBase/database.json",
-      JSON.stringify(parseData, null, 4),
-      (e) => {
-        if (e) {
-          console.log(e);
-        }
-        res.send(body);
-      },
-    );
+    if (isUrlValid(body.url)) {
+      if (isValidHostname(body.url)) {
+        parseData.push(body);
+        fs.writeFile(
+          "./DataBase/database.json",
+          JSON.stringify(parseData, null, 4),
+          (e) => {
+            if (e) {
+              console.log(e);
+            }
+            res.send(body);
+          },
+        );
+        res.send("invalid hostname");
+      }
+    }
+    res.send("invalid url");
   }
 });
 
