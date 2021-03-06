9b76d4ff9586d71bd46cf9457a4e84407263303a
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-03-06 17:25:39 +0200

    separate the database class and reorganize

--- app.js
+++ app.js
@@ -4,55 +4,15 @@ const cors = require("cors");
 const app = express();
 const fs = require("fs");
 const isValidHostname = require("is-valid-hostname");
-// const db = require("databaseClass.js");
+const { uuid } = require("uuidv4");
+const DataBase = require("./databaseClass");
 
-class DataBase {
-  constructor(location) {
-    this.db = JSON.parse(fs.readFileSync(location));
-  }
-
-  //   getDataBase() {
-  //     const dataBase = fs.readFileSync("./DataBase/database.json");
-  //     return JSON.parse(dataBase);
-  //   }
-
-  getById(req, res) {
-    const { shortUrl } = req.params;
-    const url = this.db.find((element) => element.id === shortUrl);
-    if (url) {
-      res.redirect(url.url);
-    }
-    res.send("<h1>No shortened url found in the database</h1>");
-  }
-
-  postInData(res, req) {
-    this.db.push(body);
-    fs.writeFileSync("./DataBase/database.json", JSON.stringify(db));
-  }
-
-  isExist(url) {
-    if (this.db.find((element) => element.url === url)) {
-      console.log("truth");
-      return true;
-    }
-    console.log("false");
-    return false;
-  }
-
-  isUrlValid(url) {
-    const res = url.match(
-      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
-    );
-    if (res == null) return false;
-    else return true;
-  }
-}
 const db = new DataBase("./DataBase/database.json");
 
-let ID = 0;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.static("public"));
 
 app.use("/public", express.static(`./public`));
 
@@ -73,36 +33,8 @@ app.get("/api/shorturl/:shortUrl", (req, res) => {
 });
 
 app.post("/DataBase/database.json", (req, res) => {
-  const db = new DataBase();
-  const { body } = req;
-  if (db.isExist(body.url)) {
-    res.send("url already exist!");
-  } else {
-    if (isUrlValid(body.url)) {
-      if (isValidHostname(body.url)) {
-        body.id = 2;
-        // ID += 1;
-        // parseData.push(body);
-        // fs.writeFile(
-        //   "./DataBase/database.json",
-        //   JSON.stringify(parseData, null, 4),
-        //   (e) => {
-        //     if (e) {
-        //       console.log(e);
-        //     }
-        //   },
-        // );
-        db.postInData(body);
-        res.send(body);
-      }
-      res.send("invalid hostname");
-    }
-    res.send("invalid url");
-  }
+  const id = uuid();
+  db.postInData(req, res, id);
 });
 
-// const test = new DataBase();
-// test.readFile(testtest);
-// console.log(crazy);
-// console.log("should see it after");
 module.exports = app;
--- databaseClass.js
+++ databaseClass.js
@@ -1,8 +1,8 @@
-fs = require("fs");
-
-class DataBase {
+module.exports = class DataBase {
   constructor(location) {
-    this.db = JSON.parse(fs.readFileSync(location));
+    this.fs = require("fs");
+    this.isValidHostname = require("is-valid-hostname");
+    this.database = JSON.parse(this.fs.readFileSync(location));
   }
 
   //   getDataBase() {
@@ -10,17 +10,51 @@ class DataBase {
   //     return JSON.parse(dataBase);
   //   }
 
-  getById(res, req) {
-    return this.db.find((element) => element.id === id);
+  getById(req, res) {
+    console.log(this.database);
+    const { shortUrl } = req.params;
+    const url = this.database.db.find((element) => element.id === shortUrl);
+    if (url) {
+      res.redirect(url.url);
+    }
+    res.send("<h1>No shortened url found in the database</h1>");
   }
 
-  postInData(res, req) {
-    this.db.push(body);
-    fs.writeFileSync("./DataBase/database.json", JSON.stringify(db));
+  postInData(req, res, id) {
+    const { body } = req;
+    if (this.isExist(body.url)) {
+      res.send("url already exist!");
+    } else {
+      if (this.isUrlValid(body.url)) {
+        if (this.isValidHostname(body.url)) {
+          body.id = id;
+          // ID += 1;
+          // parseData.push(body);
+          // fs.writeFile(
+          //   "./DataBase/database.json",
+          //   JSON.stringify(parseData, null, 4),
+          //   (e) => {
+          //     if (e) {
+          //       console.log(e);
+          //     }
+          //   },
+          // );
+          // database.postInData(body);
+          this.database.db.push(body);
+          this.fs.writeFileSync(
+            "./DataBase/database.json",
+            JSON.stringify(this.database),
+          );
+          res.send(body);
+        }
+        res.send("invalid hostname");
+      }
+      res.send("invalid url");
+    }
   }
 
   isExist(url) {
-    if (this.db.find((element) => element.url === url)) {
+    if (this.database.db.find((element) => element.url === url)) {
       console.log("truth");
       return true;
     }
@@ -28,10 +62,11 @@ class DataBase {
     return false;
   }
 
-  //   async writeFile(){
-  //       const dataBase=this.readFile();
-  //       console.lo
-  //   }
-}
-
-module.exports = DataBase;
+  isUrlValid(url) {
+    const res = url.match(
+      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
+    );
+    if (res == null) return false;
+    else return true;
+  }
+};
